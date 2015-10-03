e0c2f9ff80ea8b406fef2fd3db05bb9e6f592a3b
Simon Vocella <voxsim@gmail.com>
2015-10-03 13:03:20 +0200

    added some cases where players Win or have Advantage

--- game/ScoreTranslator.php
+++ game/ScoreTranslator.php
@@ -6,21 +6,24 @@ class ScoreTranslator {
         $player2Score = $player2->score();
         $player1Name = $player1->name();
         $player2Name = $player2->name();
+        $playersScore = $player1Score + $player2Score;
 
-        if ($player1Score == $player2Score && $player1Score > 3)
-            return "Deuce";
+        if($playersScore > 7) {
+            if (($player1Score - $player2Score) == 0)
+                return "Deuce";
 
-        if ($player1Score >= 4 && $player2Score >= 0 && ($player1Score - $player2Score) >= 2)
-            return "Win for " . $player1Name;
+            if (($player1Score - $player2Score) == 2)
+                return "Win for " . $player1Name;
 
-        if ($player2Score >= 4 && $player1Score >= 0 && ($player2Score - $player1Score) >= 2)
-            return "Win for " . $player2Name;
+            if (($player2Score - $player1Score) == 2)
+                return "Win for " . $player2Name;
 
-        if ($player1Score > $player2Score && $player2Score >= 3)
-            return "Advantage " . $player1Name;
+            if (($player1Score - $player2Score) == 1)
+                return "Advantage " . $player1Name;
 
-        if ($player2Score > $player1Score && $player1Score >= 3)
-            return "Advantage " . $player2Name;
+            if (($player2Score - $player1Score) == 1)
+                return "Advantage " . $player2Name;
+        }
 
         $score =  new String($player1Score.'-'.$player2Score);
         return $score
@@ -32,6 +35,15 @@ class ScoreTranslator {
             ->replace('Fifteen-Fifteen', 'Fifteen-All')
             ->replace('Thirty-Thirty', 'Thirty-All')
             ->replace('Forty-Forty', 'Deuce')
+            ->replace('4-4', "Deuce")
+            ->replace('4-Love', "Win for " . $player1Name)
+            ->replace('4-Fifteen', "Win for " . $player1Name)
+            ->replace('4-Thirty', "Win for " . $player1Name)
+            ->replace('4-Forty', "Advantage " . $player1Name)
+            ->replace('Love-4', "Win for " . $player2Name)
+            ->replace('Fifteen-4', "Win for " . $player2Name)
+            ->replace('Thirty-4', "Win for " . $player2Name)
+            ->replace('Forty-4', "Advantage " . $player2Name)
             ->value();
     }
 }
