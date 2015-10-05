73760f84ffbf565932c2d79c84df1bc99b3f4e27
Simon Vocella <voxsim@gmail.com>
2015-10-05 17:24:56 +0200

    extract translate points in a separate function in ScoreTranslator

--- game/ScoreTranslator.php
+++ game/ScoreTranslator.php
@@ -3,14 +3,18 @@
 class ScoreTranslator {
     public function translate($player1Score, $player2Score) {
         $score =  new String($player1Score.'-'.$player2Score);
-        return $score
-            ->replace('0', 'Love')
-            ->replace('1', 'Fifteen')
-            ->replace('2', 'Thirty')
-            ->replace('3', 'Forty')
+        return $this->translatePoints($score)
             ->replace('Love-Love', 'Love-All')
             ->replace('Fifteen-Fifteen', 'Fifteen-All')
             ->replace('Thirty-Thirty', 'Thirty-All')
             ->replace('Forty-Forty', 'Deuce');
+    }
+
+    private function translatePoints($score) {
+        return $score
+            ->replace('0', 'Love')
+            ->replace('1', 'Fifteen')
+            ->replace('2', 'Thirty')
+            ->replace('3', 'Forty');
     }
 }
