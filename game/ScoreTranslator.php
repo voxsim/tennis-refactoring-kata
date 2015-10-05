ce5b85e099d6735cb805e6ef897f6cb15aab6a23
Simon Vocella <voxsim@gmail.com>
2015-10-05 17:42:31 +0200

    extract second player winning/advantage score logic in a separate function

--- game/ScoreTranslator.php
+++ game/ScoreTranslator.php
@@ -6,12 +6,8 @@ class ScoreTranslator {
         $score = $this->translatePoints($score);
         $score = $this->translateFairPlay($score);
         $score = $this->translateWinningForTheFirstPlayer($score);
-        return $score
-            ->replace('4-Forty', "Advantage player1")
-            ->replace('Love-4', "Win for player2")
-            ->replace('Fifteen-4', "Win for player2")
-            ->replace('Thirty-4', "Win for player2")
-            ->replace('Forty-4', "Advantage player2");
+        $score = $this->translateWinningForTheSecondPlayer($score);
+        return $score;
     }
 
     private function translatePoints($score) {
@@ -35,6 +31,15 @@ class ScoreTranslator {
         return $score
             ->replace('4-Love', "Win for player1")
             ->replace('4-Fifteen', "Win for player1")
-            ->replace('4-Thirty', "Win for player1");
+            ->replace('4-Thirty', "Win for player1")
+            ->replace('4-Forty', "Advantage player1");
+    }
+
+    private function translateWinningForTheSecondPlayer($score) {
+        return $score
+            ->replace('Love-4', "Win for player2")
+            ->replace('Fifteen-4', "Win for player2")
+            ->replace('Thirty-4', "Win for player2")
+            ->replace('Forty-4', "Advantage player2");
     }
 }
