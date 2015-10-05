263c0401b0259a77ab227aa78f563cf5ef6d3b47
Simon Vocella <voxsim@gmail.com>
2015-10-05 17:23:16 +0200

    move players dependency from scoreTranslator back to TennisGameImpl

--- TennisGameImpl.php
+++ TennisGameImpl.php
@@ -3,19 +3,45 @@
 class TennisGameImpl implements TennisGame
 {
     private $players;
+    private $scoreTranslator;
 
     public function __construct($player1Name, $player2Name)
     {
-        $this->player1 = Player::create($player1Name);
-        $this->player2 = Player::create($player2Name);
-        $this->players[$player1Name] = $this->player1;
-        $this->players[$player2Name] = $this->player2;
+        $this->players[0] = $this->players[$player1Name] = Player::create($player1Name);
+        $this->players[1] = $this->players[$player2Name] = Player::create($player2Name);
         $this->scoreTranslator = new ScoreTranslator();
     }
 
     public function getScore()
     {
-        return $this->scoreTranslator->translate($this->player1, $this->player2);
+        $player1Score = $this->players[0]->score();
+        $player2Score = $this->players[1]->score();
+        $player1Name = $this->players[0]->name();
+        $player2Name = $this->players[1]->name();
+        $playersScore = $player1Score + $player2Score;
+
+        if($playersScore > 8) {
+            $playersDiff = $player1Score - $player2Score;
+            $score = array();
+            $score[0] = "Deuce";
+            $score[1] = "Advantage " . $player1Name;
+            $score[-1] = "Advantage " . $player2Name;
+            $score[2] = "Win for " . $player1Name;
+            $score[-2] = "Win for " . $player2Name;
+            return $score[$playersDiff];
+        }
+
+        return $this->scoreTranslator->translate($player1Score, $player2Score)
+            ->replace('4-Love', "Win for " . $player1Name)
+            ->replace('4-Fifteen', "Win for " . $player1Name)
+            ->replace('4-Thirty', "Win for " . $player1Name)
+            ->replace('4-Forty', "Advantage " . $player1Name)
+            ->replace('4-4', "Deuce")
+            ->replace('Love-4', "Win for " . $player2Name)
+            ->replace('Fifteen-4', "Win for " . $player2Name)
+            ->replace('Thirty-4', "Win for " . $player2Name)
+            ->replace('Forty-4', "Advantage " . $player2Name)
+            ->value();
     }
 
     public function wonPoint($player)
--- game/ScoreTranslator.php
+++ game/ScoreTranslator.php
@@ -1,24 +1,7 @@
 <?php
 
 class ScoreTranslator {
-    public function translate($player1, $player2) {
-        $player1Score = $player1->score();
-        $player2Score = $player2->score();
-        $player1Name = $player1->name();
-        $player2Name = $player2->name();
-        $playersScore = $player1Score + $player2Score;
-
-        if($playersScore > 8) {
-            $playersDiff = $player1Score - $player2Score;
-            $score = array();
-            $score[0] = "Deuce";
-            $score[1] = "Advantage " . $player1Name;
-            $score[-1] = "Advantage " . $player2Name;
-            $score[2] = "Win for " . $player1Name;
-            $score[-2] = "Win for " . $player2Name;
-            return $score[$playersDiff];
-        }
-
+    public function translate($player1Score, $player2Score) {
         $score =  new String($player1Score.'-'.$player2Score);
         return $score
             ->replace('0', 'Love')
@@ -28,16 +11,6 @@ class ScoreTranslator {
             ->replace('Love-Love', 'Love-All')
             ->replace('Fifteen-Fifteen', 'Fifteen-All')
             ->replace('Thirty-Thirty', 'Thirty-All')
-            ->replace('Forty-Forty', 'Deuce')
-            ->replace('4-Love', "Win for " . $player1Name)
-            ->replace('4-Fifteen', "Win for " . $player1Name)
-            ->replace('4-Thirty', "Win for " . $player1Name)
-            ->replace('4-Forty', "Advantage " . $player1Name)
-            ->replace('4-4', "Deuce")
-            ->replace('Love-4', "Win for " . $player2Name)
-            ->replace('Fifteen-4', "Win for " . $player2Name)
-            ->replace('Thirty-4', "Win for " . $player2Name)
-            ->replace('Forty-4', "Advantage " . $player2Name)
-            ->value();
+            ->replace('Forty-Forty', 'Deuce');
     }
 }
