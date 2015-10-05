4a283bb3ada8f581bf29315eba5661f3a1db5dc8
Simon Vocella <voxsim@gmail.com>
2015-10-05 17:34:22 +0200

    move 4-4 (Deuce) to translateFairPlay

--- game/ScoreTranslator.php
+++ game/ScoreTranslator.php
@@ -9,7 +9,6 @@ class ScoreTranslator {
             ->replace('4-Fifteen', "Win for player1")
             ->replace('4-Thirty', "Win for player1")
             ->replace('4-Forty', "Advantage player1")
-            ->replace('4-4', "Deuce")
             ->replace('Love-4', "Win for player2")
             ->replace('Fifteen-4', "Win for player2")
             ->replace('Thirty-4', "Win for player2")
@@ -29,6 +28,7 @@ class ScoreTranslator {
             ->replace('Love-Love', 'Love-All')
             ->replace('Fifteen-Fifteen', 'Fifteen-All')
             ->replace('Thirty-Thirty', 'Thirty-All')
-            ->replace('Forty-Forty', 'Deuce');
+            ->replace('Forty-Forty', 'Deuce')
+            ->replace('4-4', "Deuce");
     }
 }
